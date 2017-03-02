c669b1f89a75a36a919f3bbcc3bd6952678e22a8
oivheg <oivheg@gmail.com>
2017-03-02 09:32:47 +0100

    Created the project and added User

---
+++ ResturantBusserAPI/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ResturantBusserAPI.Models
+{
+    public class User
+    {
+        public String UserId { get; set; }
+        public String UserName { get; set; }
+        public int MasterID { get; set; }
+        public int Active { get; set; }
+
+    }
+}
